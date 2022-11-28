7338266c8db60431b7f36a40c9390e2d2d6089f1
harutoakira <thaidangminhhoang19@gmail.com>
2022-11-28 13:54:25 +0700

    UpdateChatBox

--- bookstore/src/components/ChatRoom/ChatBox.js
+++
@@ -1,1 +0,0 @@
-//create chat box online javascript
--- bookstore/src/pages/WishList/wishlist.js
+++ bookstore/src/pages/WishList/wishlist.js
@@ -1,42 +1,14 @@
 import React from 'react'
-import '../../../node_modules/bootstrap/dist/css'
+// import '../../../node_modules/bootstrap/dist/css'
 import Header from '../../components/Header'
 import Banner from '../../assets/advertiseBanner.png'
 import Footer from '../../components/Footer'
 import "./wishlist.css"
 export default function WishListHomePage() {
-
-    <div>
-         <Header />
-         <img src={Banner} style={{ "margin": "4vh" }} alt="banner" />
-         <Footer />
-         <ul id="items">
-            <li class="element">Javascript<button class="trash delete">X</button> </li>
-            <li class="element">Angular <button class="trash delete">X</button> </li>
-            <li class="element">Vuejs<button class="trash delete">X</button> </li>
-            <li class="element">Html & Css<button class="trash delete">X</button> </li>
-        </ul>
-        <form action="#" id="addForm">
-            <input type="text" id="item" placeholder="Add" class="add"/>
-            <input type="submit" class="btn-dark" id="button1" value="+"/>
-            <button type="submit" class="btn-dark" id="button1" >
-                <i class="material-icons">+</i>
-            </button>
-        </form>
-    </div>
-
     let form = document.getElementById('addForm');
     let itemList = document.getElementById('items');
     let filter = document.getElementById('filter');
-    
-    filter.addEventListener('keyup', filterItem);
-    
-    // Call function 
-    form.addEventListener('submit', runEvent);
-    
-    let deleteBtn = document.getElementById('items');
-        deleteBtn.addEventListener('click', removeItem);
-    
+
     // Filter Items
     function filterItem(e){
         let text = e.target.value.toLowerCase();
@@ -45,19 +17,15 @@ export default function WishListHomePage() {
         let items = itemList.getElementsByTagName('li');
         Array.from(items).forEach(function(item){
             let itemArray = item.firstChild.textContent;
-            if(itemArray.toLocaleLowerCase().indexOf(text) != -1){
+            if(itemArray.toLocaleLowerCase().indexOf(text) !== -1){
                 item.style.display = 'block';
             }
             else{
                 item.style.display = 'none';
             }
         })
-        
-    
-    
     }
-    
-    // Add item function
+// Add item function
     function runEvent(e){
         e.preventDefault();
     
@@ -83,25 +51,55 @@ export default function WishListHomePage() {
         button.appendChild(document.createTextNode('X'));
     
         //add classes to button;
-        button.className = " trash delete";
+        button.className = "trash delete";
     
         // push button to li
         li.appendChild(button);
     
     }
     
+
     // function remove item
-    
-    function removeItem(e){
+function removeItem(e){
         if(e.target.classList.contains('delete')){
-            // eslint-disable-next-line no-restricted-globals
-            if(confirm("are you sure")){
+            if(window.confirm("are you sure")){
                 let li = e.target.parentElement;
                 itemList.removeChild(li);
             }
         }
         console.log(e.target.parentElement);
     }
+
+    // filter.addEventListener('keyup', filterItem(itemList));
+    
+    // // Call function 
+    // form.addEventListener('submit', runEvent);
+    
+    let deleteBtn = document.getElementById('items');
+        // deleteBtn.addEventListener('click', removeItem);
+    
+    
+    return(
+        <div>
+        <Header />
+        <img src={Banner} style={{ "margin": "4vh" }} alt="banner" />
+        <ul id="items">
+           <li class="element">Javascript<button class="trash delete">X</button> </li>
+           <li class="element">Angular <button class="trash delete">X</button> </li>
+           <li class="element">Vuejs<button class="trash delete">X</button> </li>
+           <li class="element">Html & Css<button class="trash delete">X</button> </li>
+       </ul>
+       <form action="#" id="addForm">
+           <input type="text" id="item" placeholder="Add" class="add"/>
+           <input type="submit" class="btn-dark" id="button1" value="+"/>
+           <button type="submit" class="btn-dark" id="button1" >
+               <i class="material-icons">+</i>
+           </button>
+       </form>
+       <Footer />
+
+        </div>
+    )
     
 }
 
